cb1d3734a1378da90b8c1792b2a32c0d3e717961
szatolokin <staszatolokin28@gmail.com>
2020-10-21 17:54:14 +0300

    [UPDATE] Preloader

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,3 +4,27 @@ import App from "./main.js";
 $(document).ready(function() {
   let app = new App();
 });
+
+window.onload = function (event) {
+  // работа прелоадера проверена с помощью цикла for на 100000 итераций (небольшая задержка)
+  // запущенного на событии jquery ready
+  closePreloader();
+};
+
+function closePreloader() {
+  const preloader = $(".preloader");
+
+  // если preloader изначально не закрыт
+  if (!preloader.hasClass(".preloader--closed")) {
+    // добавляем к нему класс с transition'ом закрытия
+    preloader.addClass("preloader--closing");
+
+    // т.к. duration одинакова для всех preloader item'ов
+    // то обработчик конца анимации закрытия достаточно "повесить" на любой из них
+    const preloaderItems = preloader.find(".preloader__item");
+    preloaderItems.eq(0).on("transitionend", function (event) {
+      preloader.removeClass("preloader--closing");
+      preloader.addClass("preloader--closed");
+    });
+  }
+}
